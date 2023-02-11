3ce1e2b3b28fe7cad6acc93c61d7884fcfb5666d
kowsarahammed80 <kowsarahammed80@gmail.com>
2023-02-12 02:26:00 +0600

    project-check

--- src/Sheard/Footer/Footer.js
+++ src/Sheard/Footer/Footer.js
@@ -28,11 +28,12 @@ const Footer = () => {
           <a className="link link-hover">Privacy policy</a>
           <a className="link link-hover">Cookie policy</a>
         </div>
-        <div >
+
+        <div className="">
           <span className="footer-title">Newsletter</span>
           <div className="form-control w-72">
-            <p className="mb-10">They're halpful and funny (some times, we try)</p>
-            <div className="relative">
+            <p className="mb-10 mr-6 lg:mr-0">They're halpful and funny (some times, we try)</p>
+            <div className="relative mr-10 lg:mr-0">
               <input
                 type="text"
                 placeholder="Enter email id"
@@ -41,6 +42,21 @@ const Footer = () => {
               <button className="btn bg-black absolute top-0 right-0 rounded-l-none">
                 Sign up
               </button>
+                 
+                 <div>
+                   <h1 className="font-semibold text-gray-600 mt-10 ">FOLLOW USS ON SOCIAL MEDIA</h1>
+                   <div className="divider w-3/4 font-semibold"></div>
+                  
+                  <div className="flex justify-around">
+                     <span className="text-3xl text-gray-500"><i class="fa-brands fa-square-instagram"></i></span>
+                     <span className="text-3xl text-gray-500"><i class="fa-brands fa-facebook-f"></i></span>
+                     <span className="text-3xl text-gray-500"><i class="fa-solid fa-user-group"></i></span>
+                     <span className="text-3xl text-gray-500"><i class="fa-brands fa-youtube"></i></span>
+                     <span className="text-3xl text-gray-500"><i class="fa-brands fa-twitter"></i></span>
+                  </div>
+                    
+                 </div>
+
             </div>
           </div>
         </div>
